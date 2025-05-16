ae26707e02fefd8858e372dc55498f1e8563c81b
Cesar-Linares <u202019409@upc.edu.pe>
2025-05-15 20:42:45 -0500

    feat: Version demo de lo que sería el perfil del usuario

--- src/app/core/pages/profile-management/profile-management.component.ts
+++ src/app/core/pages/profile-management/profile-management.component.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {Tourist} from '../../model/tourist.entity';
+import {TouristService} from '../../services/tourist.service';
+import {NgForOf} from '@angular/common';
 
 @Component({
   selector: 'app-profile-management',
-  imports: [],
+  imports: [
+    NgForOf
+  ],
   templateUrl: './profile-management.component.html',
   styleUrl: './profile-management.component.css'
 })
-export class ProfileManagementComponent {
+export class ProfileManagementComponent implements OnInit {
+  tourists: Tourist[] = [];
+  constructor(private touristService: TouristService) {
 
+  }
+  ngOnInit() {
+    this.touristService.getTourists().subscribe(tourists => this.tourists = tourists);
+  }
 }
---
+++ src/app/core/services/tourist.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { environment } from '../../../environments/environment.development';
+import { Favorite} from '../model/favorite.entity';
+import {Tourist} from '../model/tourist.entity';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TouristService {
+  private baseUrl = `${environment.serverBasePath}/tourists`
+  constructor(private http: HttpClient) {}
+  getTourists(): Observable<Tourist[]> {
+    return this.http.get<Tourist[]>(`${this.baseUrl}`);
+  }
+}
