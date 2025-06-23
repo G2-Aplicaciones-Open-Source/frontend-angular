8bddaa7cb1fb8de6cb45736721748d9e8c48f8c8
Jhon Galvez <jhon.galvezchambi@gmail.com>
2025-06-23 06:43:26 -0500

    feat: add production serverBasePath

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
   serverBasePath: 'https://travel-match-api.onrender.com/'
+  // serverBasePath: 'https://travelmatch-arabafgnanbsc2hb.canadacentral-01.azurewebsites.net/api/v1'
 };
